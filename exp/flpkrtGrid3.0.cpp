5d60a3ff96e04aea773a02619bc4e97266f57c43
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2021-08-26 23:46:34 +0530

    Create flpkrtGrid3.0.cpp

---
+++ exp/flpkrtGrid3.0.cpp
@@ -0,0 +1,8 @@
+Q1) Shortest Path from source to destination with atmost k skips in a undirected graph
+Q2) Given a list of strings - tickets[], a string - drawString, and an integer - tolerance, 
+    find out how many string in tickets are substring of drawString given you can do following operations:-
+    1) change 'o' to 'a' and vice-versa any no. of times in the strings in tickets.
+    2) change 'l' to 't' and vice-versa any no. of times in the strings in tickets.
+    3) remove atmost one character from the strings in tickets
+    4) remove atmost 'tolerance' no. of characters from drawString.
+Q3) Coin Change.
