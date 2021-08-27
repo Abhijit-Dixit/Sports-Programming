6f03e6245a9355d6689caf0a2cfb5a2770d78725
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2021-08-27 20:15:44 +0530

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -4,8 +4,8 @@ int KMP(string s,string t){
     int n,m;
     n=s.length();
     m=t.length();
-    int l[m]={0};
-    lps(t,l);
+    int lps[m]={0};
+    LPS(t,lps);
     int i=0,j=0;
     int ans=0;
     while(i<n){
@@ -13,40 +13,38 @@ int KMP(string s,string t){
             i++;
             j++;
         }
+        else{
+            if(j==0) i++;
+            else j=lps[j-1];            
+        }
+        
         if(j==m){
             ans++;
-            j=l[j-1];
+            j=lps[j-1];
         }
-        else if(i<n && s[i]!=t[j]){
-            if(j==0){
-                i++;
-            }
-            else{
-                j=l[j-1];
-            }
-        }
+        
     }
     return ans;
 
 }
 
-void lps(string t,int l[]){
+void LPS(string t,int l[]){
     int m=t.length();
-    l[0]=0;
-    int len=0;
-    int i=1;
+    lps[0]=0;
+    int len=0; //pointer to last element of prefix
+    int i=1; //pointer to last element of suffix
     while(i<m){
-        if(t[i]==t[len]){
-            len++;
-            l[i]=len;
+        if(t[len]==t[i]){
+            len++; // here len is increased before assigned to lps[i] because indices are one less than length
+            lps[i]=len;
             i++;
         }
         else{
             if(len!=0){
-                len=l[len-1];
+                len=lps[len-1]; //if 'len' doesn't match then we look for second best(largest) option which is lps[len-1]; 
             }
             else{
-                l[i]=0;
+                lps[i]=0; // if len==0 then len cannot be reduced any furter, so simply increment i;
                 i++;
             }
         }
