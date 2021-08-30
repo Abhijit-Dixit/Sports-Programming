61934c17ca6ec3d7e63600cf23be925452dcba55
ABHIJIT DIXIT <abhijitdixitent@gmail.com>
2021-08-30 18:48:14 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -10,47 +10,30 @@ template<typename Head,typename...Tail>void debug_out(vector<string>args,int idx
 #else
 #define debug(...) 42
 #endif
-#define ll          long long
-#define ld          long double
-#define ull         unsigned long long
 #define pb          push_back
-#define pii         pair<int,int>
-#define pll         pair<long,long>
 #define vi          vector<int>
-#define vll         vector<ll>
-#define vii         vector<pii>
-#define Mi          map<int,int>
-#define mii         map<pii,int>
 #define all(a)      (a).begin(),(a).end()
 #define rall(a)      (a).rbegin(),(a).rend()
 #define ff           first
 #define ss           second
-#define sz(x)       (int)x.size()
 #define endl        '\n'
 #define mod        1000000007
 //#define mod        998244353
 #define rep(i,a,b)  for(int i=a;i<b;i++)
 #define rem(i,a,b)  for(int i=a;i>b;i--)
-#define mp(a,b)     make_pair(a,b)
 #define INF         numeric_limits<ll>::max()
 #define NINF        numeric_limits<ll>::min()
 #define vvi(a,b,name)    vector<vector<int>> name(a,vector<int>(b,-1))
 #define vvd(a,b,name)    vector<vector<double>> name(a,vector<double>(b,-1))
+#define int long long
 //const ld pi=3.14159265359;
-inline ll add(ll a,ll b,ll m){
-    if((a+b)>=m)return (a+b)%m;
-    return a+b;
-}
-inline ll mul(ll a,ll b,ll m){
-    if((a*b)<m)return a*b;
-    return (a*b)%m;
-}
+
 void initialize_globals(){
     return;
 }
-#define int long long
+
 void solve(){
-    
+
     initialize_globals();
 }
 
@@ -60,7 +43,7 @@ int32_t main(){
     cout.tie(0);
     long t=1;
     //cin>>t;
-    while(t--){
+    for(int i=1;i<=t;i++){
         solve();
     }
     return 0;
